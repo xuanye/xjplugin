2da266261cabc7a3ce101ccd3ec3bf19e262f87b
xuanye wang <xuanye.wan@gmail.com>
2013-10-22 15:24:51 +0800

    添加codeship持续集成

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 xjplugin
 ========
 
-代码和文档整理中...
+示例代码通过
+![CodeShip](https://www.codeship.io/projects/81b736e0-1d15-0131-0224-26fabeabc570/status)
+自动部署至[hero](http://xjplugin.herokuapp.com)
--- static/js/plugin/xjtabpanel.js
+++ static/js/plugin/xjtabpanel.js
@@ -1,1 +1,17 @@
-xjTabpanel
+;(function(window,undefined,$){
+
+  	function xjTabPanel(id,options){
+		var dfop = {
+	        items: [], //选项卡数据项 {id,text,classes,disabled,closeable,content,url,cuscall,onactive}
+	        width: 500,
+	        height: 400,
+	        showcloseall: 5, //当item数量大于5个时，显示关闭所有的按钮
+	        scrollwidth: 100, //如果存在滚动条，点击按钮次每次滚动的距离
+	        autoscroll: true, //当选项卡宽度大于容器时自动添加滚动按钮
+	        onfullscreen: false,
+	        onrestorescreen: false
+	    };
+	    var headerheight = 29;
+  	}
+
+})(window,undefined,jQuery);
